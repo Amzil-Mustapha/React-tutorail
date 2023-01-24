4bd33bcd9f83c75b37919e7003c49d424db89a06
Mustapha-Amzil <mustaphaamzil124@gmail.com>
2023-01-24 21:48:09 +0100

    some chenges added to ShortCircuit Component

--- src/components/ShortCircuit.jsx
+++ src/components/ShortCircuit.jsx
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from "react";
 const ShortCircuit = () => {
   const [text, setText] = useState("");
 
-  const a = text || "random text";
   // the text is true return the text when the text is false return the random text  (text false = empty)
-  const b = text && "rendom text";
+  const a = text || "random text";
   // the text is true return the random text when the text is false return the text
+  const b = text && "rendom text";
 
   return (
     <>
@@ -15,6 +15,9 @@ const ShortCircuit = () => {
 
       {/* when the text varaible is not empty then return the h1 */}
       {text && <h1>Some Code Here</h1>}
+
+      {/* when the text varaible is empty then return the h1 */}
+      {!text && <h1>Some Code Here</h1>}
     </>
   );
 };
